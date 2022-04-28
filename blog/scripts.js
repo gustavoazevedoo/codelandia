48e103ff24a0427be2d507a3e3ab9fe5844f8352
Gustavo Azevedo <gustavosazevedo@gmail.com>
2022-04-28 19:52:21 -0300

    primeiro desafio finalizado

---
+++ blog/scripts.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '50px',
+  duration: 2000,
+  reset: false
+});
+
+ScrollReveal().reveal('.post', { delay: 200 });
+ScrollReveal().reveal('.date', { delay: 400 });
+ScrollReveal().reveal('.title', { delay: 500 });
+ScrollReveal().reveal('.description', { delay: 600 });
